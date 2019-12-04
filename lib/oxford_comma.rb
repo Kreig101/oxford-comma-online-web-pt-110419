3404fb0e6f7a81a91822eee82beb8129e06e86e6
Craig Storer <craigstorer@gmail.com>
2019-12-04 18:44:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
 
+def oxford_comma(kiwi)
+ if kiwi.length == 1 
+    kiwi.join()
+  elsif kiwi.length == 2
+     kiwi.join(" and ")
+      elsif kiwi.length >= 3
+        string = kiwi.pop
+        kiwi << "and #{string}"
+        kiwi.join(", ")
+        
+        
+       
+         
+end   
 end
+  
+
+
+
+
